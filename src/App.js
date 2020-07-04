bae2f4af2efae51a52314b51ceed16ed786d0d3b
bhavikbhoir <bhvkbhoir95@gmail.com>
2020-07-04 14:54:07 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -17,44 +17,44 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 class App extends Component {
   render() {
     return (
-      <div>
-      <Navigation />
-      <Router>
-        <Switch>
-          <Route path="/">
-            <Home />
-          </Route>
-          <Route path="/explore">
-            <Explore />
-          </Route>
-          <Route path="/notifications">
-            <Notifications />
-          </Route>
-          <Route path="/profile">
-            <Profile />
-          </Route>
-        </Switch>
-      </Router>
-      <Footer />      
-      </div>
+      // <div>
+      // <Navigation />
+      // <Router>
+      //   <Switch>
+      //     <Route path="/">
+      //       <Home />
+      //     </Route>
+      //     <Route path="/explore">
+      //       <Explore />
+      //     </Route>
+      //     <Route path="/notifications">
+      //       <Notifications />
+      //     </Route>
+      //     <Route path="/profile">
+      //       <Profile />
+      //     </Route>
+      //   </Switch>
+      // </Router>
+      // <Footer />      
+      // </div>
 
-      //  <BrowserRouter>
-      //   <div>
-      //     <Navigation />
-      //       <Switch>
-      //        <Route path="/" component={Home}/>
-      //        <Route path="/explore" component={Explore}/>
-      //        <Route path="/notifications" component={Notifications}/>
-      //        <Route path="/profile" component={Profile}/>
-      //        <Route path="/options" component={Options}/>
-      //        <Route path="/settings" component={Settings}/>
-      //        <Route path="/login" component={Login}/>
-      //        <Route path="/signup" component={Signup}/>
-      //        <Route component={Error}/>
-      //      </Switch>
-      //      <Footer />
-      //   </div> 
-      // </BrowserRouter>
+       <BrowserRouter>
+        <div>
+          <Navigation />
+            <Switch>
+             <Route path="/" component={Home}/>
+             <Route path="/explore" component={Explore}/>
+             <Route path="/notifications" component={Notifications}/>
+             <Route path="/profile" component={Profile}/>
+             <Route path="/options" component={Options}/>
+             <Route path="/settings" component={Settings}/>
+             <Route path="/login" component={Login}/>
+             <Route path="/signup" component={Signup}/>
+             <Route component={Error}/>
+           </Switch>
+           <Footer />
+        </div> 
+      </BrowserRouter>
     );
   }
 }
